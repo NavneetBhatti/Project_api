2aead947e59b449f32a7ab035001f4c676471b99
NavneetBhatti <navneetbhatti13@gmail.com>
2022-03-29 00:41:12 -0400

    ..

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const dotenv = require('dotenv').config();
 const carRoute = require('./routes/carRoutesDB');
 const userRoute = require('./routes/userRoutes');
 const authRoute = require('./routes/authRoutes');
+const cors = require('cors');
+
+
 const app = express();
+app.use(cors());
+
 const connectDB = require('./config/connectDB');
 //connect to db
 connectDB();
